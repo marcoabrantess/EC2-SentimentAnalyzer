3090d89f09acacf542d2702751b4ce9f05d54a37
Marco Aurélio <marco.abrantes@ufv.br>
2025-03-22 23:22:38 -0300

    fix s3 name

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ home_dir = os.environ.get('HOME', '/home/marco/')
 
 client = boto3.client("s3")
 response = client.list_objects_v2(
-    Bucket="raw-comments-ec2-sentiment-analyzer-sa-east-1-marcoabrantes",
+    Bucket="raw-comments-sa1-marcoabrantes",
 )
+
 print(response)
