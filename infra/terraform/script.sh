8d31276943b0667de5f1020a2dc2980b7d34d2d2
Marco Aurélio <marco.abrantes@ufv.br>
2025-03-22 23:22:18 -0300

    update script

--- infra/terraform/script.sh
+++ infra/terraform/script.sh
@@ -12,12 +12,18 @@ systemctl enable docker
 echo "Docker installed"
 
 echo "Logging in to ECR"
-aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 383498687630.dkr.ecr.sa-east-1.amazonaws.com
+aws ecr get-login-password --region sa-east-1 | sudo docker login --username AWS --password-stdin 383498687630.dkr.ecr.sa-east-1.amazonaws.com
 echo "Logged in to ECR"
 
 echo "Pulling latest image from ECR"
-docker pull 383498687630.dkr.ecr.sa-east-1.amazonaws.com/sentiment-analyzer:latest
+sudo docker pull 383498687630.dkr.ecr.sa-east-1.amazonaws.com/sentiment-analyzer:latest
 
-echo "Running container"
-docker run -d --name sentiment-analyzer 383498687630.dkr.ecr.sa-east-1.amazonaws.com/sentiment-analyzer:latest
+echo "Removing old container if it exists"
+sudo docker rm -f sentiment-analyzer || true
+
+echo "Running new container"
+sudo docker run -d --name sentiment-analyzer 383498687630.dkr.ecr.sa-east-1.amazonaws.com/sentiment-analyzer:latest
 echo "Container is running"
+
+echo "Container logs:"
+sudo docker logs sentiment-analyzer
